d8b9b93a46000ca5bc5248b0f2436d81f5f5725a
XiaoWeiTo <2254351821@qq.com>
2019-05-26 15:00:48 +0800

    Add secure password and attribute to users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true,length:{minumum:5,maximum:40}
   validates :description, presence: true,length:{minumum:10,maximum:200}
+  validates :user_id,presence: true
 end
